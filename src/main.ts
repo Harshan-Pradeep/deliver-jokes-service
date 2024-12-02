79e57ddc427099171c43ed53577effd05a4a183c
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2024-12-02 16:47:00 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://generaterandomjokes.netlify.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
