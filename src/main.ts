972682f36c30186c5c8c878635e977bab8e7c99d
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2024-12-02 18:55:08 +0530

    Remove cors configs

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://generaterandomjokes.netlify.app'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-  });
+  app.enableCors();
   
 
   app.useGlobalPipes(new ValidationPipe({
